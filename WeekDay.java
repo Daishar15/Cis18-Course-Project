ef517f4b0354a59599c197603ff47d4db153a0b5
Daishar15 <drandolph4@student.rccd.edu>
2025-02-13 00:57:35 -0800

    Add files via upload

---
+++ WeekDay.java
@@ -0,0 +1,62 @@
+
+nano shoes.java
+import java.util.Map;
+
+class Shoes { 
+  public static void main(String[] args) {
+
+  int aj1orange; //defining the variables
+  int aj1blue;
+  int aj1red;
+  int aj3pink;
+  int aj3red;
+  int aj4white;
+  }
+}
+class ShoePrice {
+
+  public ShoePrice() {
+      ShoePrice("aj1orange", 150); // Creating the ShoePrice method
+      ShoePrice.map("aj1blue", 150); 
+      ShoePrice.map("aj1red", 150);
+      ShoePrice.map("aj3pink", 200);
+      ShoePrice.map("aj3 ed", 200);
+      ShoePrice.map("aj4white", 300);
+  }
+}
+class ShoeReleaseDate {
+  public ShoeReleaseDate(){
+  }
+}
+public class WeekDay { // setting the array for the days of the week that the shoes are able to be purchased
+    private int[] Weekdaynumber = {1, 2, 3, 4, 5, 6, 7};
+    private int Weekday;
+
+    public class WeekDay { // defining the array
+      private String[] shoesOfWeek = {"aj1orange", "aj1blue", "aj1red", "aj3pink", "aj3red", "aj4white"}
+      
+      public void Purchase(String Shoes, int quantity) {
+        if (ShoePrice.containsKey(Shoes)) {
+            int totalPrice = prices.get(Shoes) * quantity;
+            System.out.println("Congrats, your shoes are confirmed! Price: $" + totalPrice);
+        } else {
+            System.out.println("Try again next time.");
+
+
+    public void displayWeekday() { //setting switch statements to print the days of the week.
+        String  = "";
+        switch (assignedDay) {
+            case 1: aj1orange = "Monday"; break;
+            case 2: aj1blue = "Tuesday"; break;
+            case 3: aj1red = "Wednesday"; break;
+            case 4: aj3pink = "Thursday"; break;
+            case 5: aj3red = "Friday"; break;
+            case 6: aj4white = "Saturday"; break;
+             
+            }
+      }
+  
+    }
+  }
+    }
+    }
